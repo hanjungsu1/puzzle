51965f9ce467db6cc9070b06689c8e9af7da86ce
한정수 <wjdtn1642@naver.com>
2024-07-03 21:40:20 +0900

    클릭한 버튼이 이동 가능하도록 설정

--- math.js
+++ math.js
@@ -20,3 +20,56 @@ function ClearStatus() {
     }
     complete = false;
 }
+
+function CheckComplete() {
+    i=1;
+    while (i < 9) {
+        if (document.all['b'+ i].value == (' ' + i + ' ')) i = i + 1;
+        else return;
+    }
+    alert('축하합니다.');
+    complete = true;
+}
+function Move(pos, value) {
+    if (complete == ture) return;
+    if ( (pos % 3) == 2 || (pos % 3) == 0) {
+        newpos = parselnt(pos) - 1;
+        if (document.all['b' + newpos].value == ' x') {
+            tempvalue = document.all['b'+newpos].value;
+            document.all['b'+newpos].value = value;
+            document.all['b'+pos].value = tempvalue;
+            CheckComplete();
+            return;
+        }
+    }
+    if ( (pos % 3) == 1 || (pos % 3) == 2) {
+        newpos = parselnt(pos) + 1;
+        if (document.all['b' + newpos].value == ' x') {
+            tempvalue = document.all['b'+newpos].value;
+            document.all['b'+newpos].value = value;
+            document.all['b'+pos].value = tempvalue;
+            CheckComplete();
+            return;
+        }
+    }
+    if (parseInt(pos) > 3) {
+        newpos = parselnt(pos) - 3;
+        if (document.all['b' + newpos].value == ' x') {
+            tempvalue = document.all['b'+newpos].value;
+            document.all['b'+newpos].value = value;
+            document.all['b'+pos].value = tempvalue;
+            CheckComplete();
+            return;
+        }
+    }
+    if (parseInt(pos) < 7) {
+        newpos = parselnt(pos) + 3;
+        if (document.all['b' + newpos].value == ' x') {
+            tempvalue = document.all['b'+newpos].value;
+            document.all['b'+newpos].value = value;
+            document.all['b'+pos].value = tempvalue;
+            CheckComplete();
+            return;
+        }
+    }
+}
